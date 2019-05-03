e7f560576728763dda774e55eeb03157a54ede80
toskatlt <to_Ska@mail.ru>
2019-05-03 18:08:52 +0400

    Ready homework

--- dz7/sql/brand.sql
+++ dz7/sql/brand.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Апр 26 2019 г., 15:23
+-- Время создания: Май 03 2019 г., 18:08
 -- Версия сервера: 5.5.62-0+deb8u1
 -- Версия PHP: 5.6.40-0+deb8u1
 
@@ -31,29 +31,29 @@ CREATE TABLE IF NOT EXISTS `basket` (
   `id_session` varchar(255) NOT NULL,
   `id_goods` int(22) NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `basket`
 --
 
 INSERT INTO `basket` (`id`, `id_session`, `id_goods`, `quantity`) VALUES
-(1, 'qvdopoashm4s1g7vgd0j04vni2', 3, 3),
-(2, 'qvdopoashm4s1g7vgd0j04vni2', 2, 1),
-(3, 'qvdopoashm4s1g7vgd0j04vni2', 4, 1),
-(7, 'j5fbhjhj6o4blp7kv6ro9s8td1', 3, 3),
-(8, 'j5fbhjhj6o4blp7kv6ro9s8td1', 4, 4),
-(10, 'j5fbhjhj6o4blp7kv6ro9s8td1', 2, 1),
-(14, 'iiios0tb8vpeau287u4a4sp2k1', 2, 1),
-(15, 'iiios0tb8vpeau287u4a4sp2k1', 6, 1),
-(16, 'iiios0tb8vpeau287u4a4sp2k1', 4, 2),
-(18, 'ukbh5hs0i11f0iipntodvfrmk1', 3, 1),
-(19, 'ukbh5hs0i11f0iipntodvfrmk1', 4, 1),
-(20, 'ukbh5hs0i11f0iipntodvfrmk1', 2, 1),
-(21, 'ukbh5hs0i11f0iipntodvfrmk1', 1, 2),
-(24, 'j5fbhjhj6o4blp7kv6ro9s8td1', 7, 1),
-(25, 's7tjegebadkfgjb69i2du0a382', 2, 1),
-(26, 's7tjegebadkfgjb69i2du0a382', 3, 1);
+(1, 'mabvrtc4611edil9tshut1k3q3', 3, 3),
+(2, 'mabvrtc4611edil9tshut1k3q3', 4, 4),
+(4, '9j828hv3is5p5pp87t48em46k4', 3, 3),
+(5, '9j828hv3is5p5pp87t48em46k4', 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -82,6 +82,27 @@ INSERT INTO `products` (`id`, `productName`, `price`, `sex`) VALUES
 (7, 'Reebok штаны', 33, 1),
 (8, 'ZARA толстовка', 38, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `hash` varchar(100) NOT NULL,
+  `access` int(2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `hash`, `access`) VALUES
+(1, 'admin', '$2y$10$GAh95KWqFf1Fw4YyH/BCnuODYbJ1Mln78vDuOIwj7WQvChhR8QcX.', '14770365015ccc1479cbec23.86659861', 1);
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -93,10 +114,22 @@ ALTER TABLE `basket`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `basket_uniq` (`id_session`,`id_goods`), ADD KEY `id_session` (`id_session`,`id_goods`);
 
 --
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`);
+
+--
 -- Индексы таблицы `products`
 --
 ALTER TABLE `products`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD KEY `login` (`login`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -106,12 +139,22 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT для таблицы `basket`
 --
 ALTER TABLE `basket`
-MODIFY `id` int(22) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+MODIFY `id` int(22) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
